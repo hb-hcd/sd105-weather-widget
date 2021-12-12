a3ff45adddb0377bce302d316d28b314fd28774d
hhb <hhbryan@126.com>
2021-12-11 22:15:22 -0600

    remove redundant code

--- src/script/app.js
+++ src/script/app.js
@@ -1,5 +1,3 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat=49.803159&lon=-97.157667&exclude=daily&appid=7b8e6d74ff3768e9ec431b99631d6835
-
 // current weather
 // https://api.openweathermap.org/data/2.5/weather?q=winnipeg&units=metric&appid=7b8e6d74ff3768e9ec431b99631d6835
 
@@ -9,7 +7,6 @@
 //weather status image
 //https://openweathermap.org/img/wn/02d@2x.png
 
-let lon, lat;
 const token = 'pk.eyJ1IjoiaHhiMTEiLCJhIjoiY2t3ejVxM3h4MGtvaDJwb2JpZHAzeTFhbCJ9.XbOXHJ5oaNHOY1IFJhJefQ'
 const geoCode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'
 const queryCondition = '.json?bbox=-97.32972%2C49.762789%2C-96.951241%2C49.994007&limit=9&proximity=-97.1596764%2C49.8132188&types=poi&access_token='
@@ -31,37 +28,51 @@ const daysRange = [
     [32, 39]  //day5
 ]
 
-// function getUserLocation() {
-//     if (!navigator.geolocation) {
-//         console.log('Geolocation is not supported by your browser');
-//     } else {
-//          navigator.geolocation.getCurrentPosition((position) => {
-//             // lat = position.coords.latitude;
-//             // lon = position.coords.longitude;
-//             return position.coords;
-//         })
-//     }
-// }
+const coords = {
+    longitude: 0,
+    latitude: 0,
+};
 
-const currentWeather = () => {
-    if (!navigator.geolocation) {
-        console.log('Geolocation is not supported by your browser');
-    } else {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude
-            fetch(`${baseUrl}weather?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`)
-                .then(res => res.json())
-                .then(data => {
-                    curr_temp.innerHTML = `${Math.round(data.main.temp)}<span>&#8451;</span>`;
-                    curr_condition.innerText = `${data.weather[0].description}`
-                    curr_img.src = `${imgUrl}${data.weather[0].icon}@2x.png`;
-                });
-        })
+async function renderCurrentWeather() {
+    const res = await fetch(`${baseUrl}weather?lat=${coords.latitude}&lon=${coords.longitude}&units=metric&appid=${apikey}`);
+    const data = await res.json();
+    curr_temp.innerHTML = `${Math.round(data.main.temp)}<span>&#8451;</span>`;
+    curr_condition.innerText = `${data.weather[0].description}`;
+    curr_img.src = `${imgUrl}${data.weather[0].icon}@2x.png`;
+
+}
+
+async function renderFiveDayForecast() {
+    const res = await fetch(`${baseUrl}forecast?lat=${coords.latitude}&lon=${coords.longitude}&units=metric&appid=${apikey}`);
+    const data = await res.json();
+    const days = randomTimeofFiveDays();
+    for (const day of days) {
+        forcastDiv.insertAdjacentHTML(
+            'beforeend',
+            `<div class="day">
+                <h3>${getWeekday(data.list[day].dt_txt)}</h3>
+                <img src="http://openweathermap.org/img/wn/${data.list[day].weather[0].icon}@2x.png" />
+                <div class="description">${data.list[day].weather[0].description}</div>
+                <div class="temp">
+                  <span class="high">${Math.round(data.list[day].main.temp_max)}℃</span>/<span class="low">${Math.round(data.list[day].main.temp_min)}℃</span>
+                </div>
+              </div>`
+        )
     }
 }
 
-
+const getWeather = () => {
+    if(!navigator.geolocation){
+        console.log('Geolocation is not supported by your browser');
+    }else{
+        navigator.geolocation.getCurrentPosition((position) => {
+            coords.longitude = position.coords.longitude;
+            coords.latitude = position.coords.latitude;
+            renderCurrentWeather();
+            renderFiveDayForecast();
+        });
+    }
+}
 
 //randomly select 5 index for 5day/3hour forecast data
 const randomTimeofFiveDays = () => {
@@ -102,37 +113,4 @@ const getWeekday = (str) => {
     return day;
 }
 
-const fiveDayForecast = () => {
-    if (!navigator.geolocation) {
-        console.log('Geolocation is not supported by your browser');
-    } else {
-        navigator.geolocation.getCurrentPosition((position) => {
-            let lat = position.coords.latitude;
-            let lon = position.coords.longitude
-            fetch(`${baseUrl}forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`)
-                .then(res => res.json())
-                .then(data => {
-                    const days = randomTimeofFiveDays();
-                    for (const day of days) {
-                        forcastDiv.insertAdjacentHTML(
-                            'beforeend',
-                            `<div class="day">
-                                <h3>${getWeekday(data.list[day].dt_txt)}</h3>
-                                <img src="http://openweathermap.org/img/wn/${data.list[day].weather[0].icon}@2x.png" />
-                                <div class="description">${data.list[day].weather[0].description}</div>
-                                <div class="temp">
-                                  <span class="high">${Math.round(data.list[day].main.temp_max)}℃</span>/<span class="low">${Math.round(data.list[day].main.temp_min)}℃</span>
-                                </div>
-                              </div>`
-                        )
-                    }
-                });
-        });
-    }
-}
-
-
-currentWeather();
-fiveDayForecast();
-
-
+window.addEventListener('load',getWeather);
